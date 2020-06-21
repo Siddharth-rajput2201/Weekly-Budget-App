284cc9ecf4061c64650faf3c4856e838875b2080
Hush <rajputsiddharth18@gmail.com>
2020-06-21 17:55:13 +0530

    Added Transaction Class

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:budgetapp/trasanction_class.dart';
 import 'package:flutter/material.dart';
 
 void main()
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Budget App",
       home: MyHomepage(),
     );
@@ -16,15 +18,30 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomepage extends StatelessWidget {
+  final List<Transaction> transaction = [
+    Transaction(id: "T1",amount: 20,date: DateTime.now(), title: "Shoes")
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Budget App"),
       ),
-      body: Center(
-        child: Text("Play Around"),
-      ),
-    );
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text("STATS!"),
+            ),
+          ),
+          Card(
+            child: Text("LIST OF TILES"),
+          ),
+        ],
+      )
+      );
   }
 }
